f7e9061c3a7c374b990051893f2681ee2866e6c8
uchiyama <r.uchiyama@sint.co.jp>
2019-09-26 22:09:19 +0900

    upate add list

--- pages/diagnosis.vue
+++ pages/diagnosis.vue
@@ -1,69 +1,30 @@
 <template>
   <v-layout>
     <v-stepper v-model="e1" vertical>
-      <v-stepper-step
-        step="1">
-        人からよく思われている人柄
-      </v-stepper-step>
-      <v-stepper-content step="1">
-        hogehoge
-        <v-btn color="primary" @click="e1=2">Next</v-btn>
-      </v-stepper-content>
-      <v-stepper-step
-        step="2">
-        自覚がある自分の傾向
-      </v-stepper-step>
-      <v-stepper-content step="2">
-        fugufugu
-      <v-btn color="normal" @click="e1=1">back</v-btn>
-      <v-btn color="primary" @click="e1=3">Next</v-btn>
-      </v-stepper-content>
-      <v-stepper-step
-        step="3">
-        人からよく思われていない所
-      </v-stepper-step>
-      <v-stepper-content step="3">
-        fugufugu
-      <v-btn color="normal" @click="e1=2">back</v-btn>
-      <v-btn color="primary" @click="e1=4">Next</v-btn>
-      </v-stepper-content>
-      <v-stepper-step
-        step="4">
-        つい、やりがちな行動
-      </v-stepper-step>
-      <v-stepper-content step="4">
-        fugufugu
-      <v-btn color="normal" @click="e1=3">back</v-btn>
-      <v-btn color="primary" @click="e1=5">Next</v-btn>
-      </v-stepper-content>
-      <v-stepper-step
-        step="5">
-        よくあること
-      </v-stepper-step>
-      <v-stepper-content step="5">
-        fugufugu
-      <v-btn color="normal" @click="e1=4">back</v-btn>
-      <v-btn color="primary" @click="e1=6">Next</v-btn>
-      </v-stepper-content>
-      <v-stepper-step
-        step="6">
-        コミュニケーションの特徴
-      </v-stepper-step>
-      <v-stepper-content step="6">
-        fugufugu
-      <v-btn color="normal" @click="e1=5">back</v-btn>
-      <v-btn color="primary" @click="e1=7">Next</v-btn>
-      </v-stepper-content>
-      <v-stepper-step
-        step="7">
-        自分を象徴するワード
-      </v-stepper-step>
-      <v-stepper-content step="7">
-        fugufugu
-      <v-btn color="normal" @click="e1=6">back</v-btn>
-      <v-btn color="primary" @click="e1=1">Next</v-btn>
-      </v-stepper-content>
-  </v-stepper>
+      <template v-for="item in  items">
+        <v-stepper-step editable v-bind:step="item.key">
+          {{ item.value }}
+        </v-stepper-step>
+        <v-stepper-content v-bind:step="item.key">
+          <v-list >
+            <v-subheader>
+              TypeItems
+            </v-subheader>
+            <draggable v-model="items" :options="{group:'{{ item.key }}'}" >
+            <template v-for="typeItem in item.typeItems" >
+              <v-list-item :key="typeItem.key">
+                <v-list-item-content>
+                  <v-list-item-title >{{ typeItem.value }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+            </draggable>
+          </v-list>
+          <v-btn v-if="item.prevKey  !== ''" outlined color="primary" @click="e1=(item.prevKey)">Prev</v-btn>
+          <v-btn v-if="item.nextKey !== ''" color="primary" @click="e1=(item.nextKey)">Next</v-btn>
+        </v-stepper-content>
+      </template>
+    </v-stepper>
   </v-layout>
 </template>
 
@@ -72,13 +33,16 @@ import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  },
+  layout: "none",
   data () {
     return {
-      e1 : 0
+      e1 : 0,
+      items: [
+        { key: "1", value: "人からよく思われている人柄" ,typeItems: [{ key: "1", value: "111A"},{ key: "2", value: "112"},{ key: "3", value: "113"},{ key: "4", value: "114"}], nextKey: "2", prevKey: ""},
+        { key: "2", value: "自覚がある自分の傾向" ,typeItems: [{ key: "1", value: "111"},{ key: "2", value: "112"},{ key: "3", value: "113"},{ key: "4", value: "114"}], nextKey: "3", prevKey: "1"},
+        { key: "3", value: "人からよく思われていない所" ,typeItems: [{ key: "1", value: "111"},{ key: "2", value: "112"},{ key: "3", value: "113"},{ key: "4", value: "114"}], nextKey: "4", prevKey: "2"},
+        { key: "4", value: "つい、やりがちな行動" ,typeItems: [{ key: "1", value: "111"},{ key: "2", value: "112"},{ key: "3", value: "113"},{ key: "4", value: "114"}], nextKey: "", prevKey: "3"}
+      ] 
     } 
   }
 }
---
+++ pages/hoge.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+    <v-stepper v-model="e1" vertical>
+        <v-stepper-step editable step="1">
+          hogehoge
+        </v-stepper-step>
+        <v-stepper-content step="1">
+          <v-list >
+            <v-subheader>
+              TypeItems
+            </v-subheader>
+            <draggable v-model="items" :options="{group:'hoge'}" >
+              <v-list-item key="1">
+                <v-list-item-content>
+                  <v-list-item-title >hogehoge</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item key="2">
+                <v-list-item-content>
+                  <v-list-item-title >fugafuga</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </draggable>
+          </v-list>
+          <v-btn color="primary" @click="e1=2">Next</v-btn>
+        </v-stepper-content>
+    </v-stepper>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  },
+  data () {
+    return {
+      e1 : 0,
+      items: [
+        { key: "1", value: "人からよく思われている人柄" ,typeItems: [{ key: "1", value: "111A"},{ key: "2", value: "112"},{ key: "3", value: "113"},{ key: "4", value: "114"}], nextKey: "2", beforeKey: ""},
+        { key: "2", value: "自覚がある自分の傾向" ,typeItems: [{ key: "1", value: "111"},{ key: "2", value: "112"},{ key: "3", value: "113"},{ key: "4", value: "114"}]},
+        { key: "3", value: "人からよく思われていない所" ,typeItems: [{ key: "1", value: "111"},{ key: "2", value: "112"},{ key: "3", value: "113"},{ key: "4", value: "114"}]},
+        { key: "4", value: "つい、やりがちな行動" ,typeItems: [{ key: "1", value: "111"},{ key: "2", value: "112"},{ key: "3", value: "113"},{ key: "4", value: "114"}]}
+      ] 
+    } 
+  }
+}
+</script>
+
